1e0fd28f8d14e44a40bec5ad8994679224db4df1
Mazin35 <mazin.dhuhli237@gmail.com>
2022-04-02 00:08:35 +0400

    time and website moving text

---
+++ www/assets/js/movingtxt.js
@@ -0,0 +1,14 @@
+
+
+function moving(){
+// to add date 
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// to print output
+document.getElementById("movingtext").innerHTML = "Welcome to Clinicom Oman website, and the time is  " + time ;
+console.log(date)
+}
+
+window.onload = function(){
+    moving();
+}
