77c733008c20cd7171feab2753529c9348828d88
0cool-design <hamoodlgg4@gmail.com>
2022-04-11 23:11:06 +0400

    main

--- www/assets/js/patients.js
+++ www/assets/js/patients.js
@@ -79,19 +79,20 @@ function searchFunction() {
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-                flag = 1;
-            } else {
-                tr[i].style.display = "none";
-                flag = 0;
-            }
-        }
-    }
-    if (flag == 0) {
-        alert("No patient found");
+        console.log(td);
+        //     if (td) {
+        //         txtValue = td.textContent || td.innerText;
+        //         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        //             tr[i].style.display = "";
+        //             flag = 1;
+        //         } else {
+        //             tr[i].style.display = "none";
+        //             flag = 0;
+        //         }
+        //     }
+        // }
+        // if (flag == 0) {
+        //     alert("No patient found");
     }
 }
 
